87b92fd6777157c59b580f48dabbeefeb661a351
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-29 20:04:41 -0400

    added colomns with products

---
+++ bamazon.sql
@@ -0,0 +1,46 @@
+-- Drops the bamazon_db if it exists currently --
+DROP DATABASE IF EXISTS bamazon_db;
+-- Creates the "bamazon_db" database --
+CREATE DATABASE bamazon_db;
+
+-- Makes it so all of the following code will affect bamazon_db --
+USE bamazon_db;
+
+-- Creates the table "products" within bamazon_db --
+CREATE TABLE products (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(30),
+  department_name VARCHAR(30),
+  price INTEGER(10),
+  stock_quanity INTEGER(10),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("Cactus Jack", "Air Jordan", 250, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("Sample Lab 1500", "New Balance", 120, 70);
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES (" React 'Brutal Honey'", "NIKE", 220, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("SB 'Walk The Dog'", "NIKE", 190, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("Future Craft Loop", "Adidas", 300, 14);
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("990v5", "New Balance", 150, 75);
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("Tailwind 'TEAM ORANGE'", "NIKE", 180, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("Yeezy Boost 350", "Adidas", 350, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("S/LAB XT-4 ADV", "Solomon", 165, 60);
+
+
