f0a4adf7b7d43bad14cc468096417df5d7645f3b
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-31 23:59:42 -0400

    added quantity search

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,19 +1,20 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-​
-var connection = mysql.createConnection({
+
+​var connection = mysql.createConnection({
   host: "localhost",
-​
+
   // Your port; if not 3306
   port: 3306,
-​
+
   // Your username
   user: "root",
-​
+
   // Your password
   password: "D1!!1ng3r",
   database: "bamazon_DB"
 });
+
 ​
 connection.connect(function(err) {
   if (err) throw err;
@@ -44,30 +45,88 @@ function runSearch() {
           break;
 
         case "exit":
+          connection.end();
           break;     
       }
     });
 
   };
-  function productSearch(){
+  function productSearch() {
     inquirer
       .prompt({
         name: "product",
         type: "input",
-        message: "What product would you like to search for?" 
+        message: "What product would you like to look for?"
       })
-      .then(function(answer){
-        var query = "SELECT id, product, brand FROM bamazon WHERE ?";
-        conection.query(query, { product: answer.product 
-      }, function(err, res){
+      .then(function(answer) {
+        console.log(answer.song);
+        connection.query("SELECT * FROM bamazon WHERE ?", { song: answer.song }, function(err, res) {
+          if (err) throw err;
+          console.log(
+            "Id: " +
+              res[0].id +
+              " || Product: " +
+              res[0].product_name +
+              " || Brand: " +
+              res[0].department_name +
+              " || Quantity: " +
+              res[0].stock_quantity
+          );
+          runSearch();
+        });
+      });
+ 
+  }
+  
+  function rangeSearch () {
+    inquirer
+    .prompt([
+      {
+      name: "start",
+      type: "input",
+      message: "Enter starting inventory amount: ",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    },
+    {
+        name: "end",
+        type:"input",
+        message: "Enter ending position: ",
+        validate: function(value) {
+          if (isNaN(value) === false){
+            return ture;
+          }
+          return false;
+        }
+      },
+    
+    ])
+    .then(function(answer) {
+      var query = "SELECT Id,product,brand,quantity FROM bamazon WHERE position BETWEEN ? AND ?";
+      connection.query(query, [answer.start, answer.end], function(err, res) {
         if (err) throw err;
-        for (var i= 0; i < res.length; i++) {
-          console.log("Id: " + res[i].id + " || Product: " + res[i].product_name + " || Brand: " + res[i].department_name);
+        for (var i = 0; i < res.length; i++) {
+          console.log(
+            "Id: " +
+              res[i].id +
+              " || Product: " +
+              res[i].prduct_name +
+              " || Brand: " +
+              res[i].department_name +
+              " || Quantity: " +
+              res[i].stock_quantity
+          );
         }
         runSearch();
       });
     });
   }
+    
+  
 
 
   
